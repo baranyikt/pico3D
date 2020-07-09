f894c209a379d626a07c9fa5bfcf78f368b1010e
Baranyi Károly <47175603+baranyikt@users.noreply.github.com>
2020-07-09 16:50:24 +0200

    bugfix: INC SI added to lineDraw

--- main.asm
+++ main.asm
@@ -20,24 +20,26 @@ lineDraw:                       ; draws line from (si,di) to (cx,dx)
     sub bx, si                  ; bx = |tox-fromx|
     shl bp, 1                   ; bp = 2*deltay
     mov ax, bp
-    sub ax, bx                  ; ax = 2*deltay - deltbp
-    shl bx, 1                   ; bx = 2*deltbp
+    sub ax, bx                  ; ax = 2*deltay - deltax
+    shl bx, 1                   ; bx = 2*deltax
     mov dl, 5
 
 ld2:
-    call plotxy
+    call plotxy					; draw (si,di), color dl
     cmp ax,0
     jle noyinc
     inc di
     sub ax, bx
 noyinc:
     add ax, bp
+	inc si
     cmp si, cx
     jbe ld2
 
     ret                         ; lineDraw: 0x55-0x1e bytes (55) 
                                 ; using the unused dx for color instead of ax: 0x51-0x1e bytes (51) 
                                 ; swapping bp & ax: same length
+                                ; bugfix: added inc si: +2 bytes, 0x53-0x1e (53)
 main:
     mov ax, 0A000h
     mov es, ax
@@ -53,3 +55,5 @@ main:
 
                                 ; total so far 81h bytes
                                 ; using dl for color & using dx in linedraw 78h bytes
+								; inc si bugfix: 7ah bytes
+	nop
