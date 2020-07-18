b7e579679964eabf856bcc782dbf10ce18af8f9f
Baranyi Károly <47175603+baranyikt@users.noreply.github.com>
2020-07-18 18:02:09 +0200

    projector function first draft

--- main.asm
+++ main.asm
@@ -139,7 +139,111 @@ skipXincNow:
 	jle lineDrawMainLoop_horzdom
 	
 	ret
+	
+plyangle:
+	dd 0
+plycoords:
+	dd 0, 0
 
+polygon: 
+	dw 0, 1
+	dw 2, 3
+	dw 4, 5
+	dw 6, 7
+	dw 8, 9
+	dw 0, 1
+POLYGONENTRY equ (2 * 2)
+POLYGONSIZE equ (($-polygon)/POLYGONENTRY) - 1
+
+	
+aspectTimesZoom: 
+	dw 160.0						; aspect ratio * zoom value = 320/200 * 100, factor of X coords
+zoomWOAspect:
+	dw 100.0						; zoom value (alone), factor of Y coords
+wallHeight:
+	dw 50.0
+	
+	
+tx1:	dd 0
+tz1:	dd 0
+x1:		dw 0
+y1top:	dw 0
+y1bot:	dw 0
+
+TEMPVARSIZE	equ ($-tx1)
+
+tx2:	dd 0
+tz2:	dd 0
+x2:		dw 0
+y2top:	dw 0
+y2bot:	dw 0
+
+	
+projector:
+	fld dword [plyangle]		; 
+	fsincos						; [ cos(plyangle), sin(plyangle) ]
+	fld dword [plycoords]		;
+	fld dword [plycoords+4]		; [ ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	lea si, [polygon]			;
+	mov cx, POLYGONSIZE
+	
+calcOneSide:
+	lea di, [tx1]				; set to ....1 variableset
+	push cx						; save cx for outer loop
+	mov cx, 2					; two vertices at a time
+calcOneVertex:	
+	fild word [si]				; [ pgn[i].x, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fsub st2					; [ pgn[i].x-ply.x, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fild word [si+2]			; [ pgn[i].y, pgn[i].x-ply.x, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fsub st2					; [ pgn[i].y-ply.y, pgn[i].x-ply.x, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+								;   ------ty------  ------tx------
+	fld st1						; [ tx, ty, tx, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fmul st5					; [ tx*cos(angle), ty, tx, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	
+	fld st1						; [ ty, tx*cos(angle), ty, tx, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fmul st7					; FULL [ ty*sin(angle), tx*cos(angle), ty, tx, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fadd						; [ tx*cos(angle)+ty*sin(angle), ty, tx, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	
+								; di+4 = tz1 or 2
+	fstp dword [di+4]			; [ ty, tx, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	
+	fmul st4					; [ ty*cos(plyangle), tx, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fxch st1					; [ tx, ty*cos(plyangle), ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fmul st5					; [ tx*sin(plyangle), ty*cos(plyangle), ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fsub						; [ tx*sin(plyangle)-ty*cos(plyangle), ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+								; di = tx1/2
+	fstp dword [di]				; [ ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	
+	fld dword [di+4]			; di+4 = tz1/2, di = tx1/2
+	fild word [aspectTimesZoom]
+	fld dword [di]				; [ tx1, aspectTimesZoom, tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fchs						; [ -tx1, aspectTimesZoom, tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fmul						; [ -tx1*aspectTimesZoom, tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fdiv st1					; [ -tx1*aspectTimesZoom/tz1, tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+								; di+8 = x1/2
+	fistp word [di+8]			; [ tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	
+	fild word [zoomWOAspect]	;
+	fild word [wallHeight]		; [ wallHeight, zoomWOAspect, tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fld st0						; FULL [ wallHeight, wallHeight, zoomWOAspect, tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fchs						; FULL [ -wallHeight, wallHeight, zoomWOAspect, tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fmul st2					; FULL [ -wallHeight*zoomWOAspect, wallHeight, zoomWOAspect, tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fdiv st3					; FULL [ -wallHeight*zoomWOAspect/tz1, wallHeight, zoomWOAspect, tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+								; di+10 = y1top/y2top, di+12 = y1bot/y2bot
+	fistp word [di+10]			; [ wallHeight, zoomWOAspect, tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fmul						; [ wallHeight*zoomWOAspect, tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fdiv						; [ wallHeight*zoomWOAspect/tz1, ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+	fistp word [di+12]			; [ ply.y, ply.x, cos(plyangle), sin(plyangle) ]
+								; FPU stack ready for next iteration
+
+	add si, POLYGONENTRY		; move to next vertex
+	add di, TEMPVARSIZE			; move to ...2 variableset
+	loop calcOneVertex
+	
+	pop cx
+	sub si, POLYGONENTRY		; two steps forward minus one step back = one step fwd
+	loop calcOneSide
+	
 main:
 	mov ax, 0A000h
 	mov es, ax
@@ -156,6 +260,10 @@ main:
 	nop							; end
 	
 ; stats
-; plotxy		01h-18h		(23)
-; lineDraw		18h-a1h		(137)
+; plotxy			01h-18h		(23)
+; lineDraw			18h-a1h		(137)
+; projector vars	a1h-e7h		(70)
+; projector			e7h-15eh	(119)
+; main				15eh-17ch	(30)
+; total							380 bytes
 ; total			0h-0bfh		(191)
